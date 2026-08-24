e78c67f5693041bbfca1da5b5dfe97feb46fcc19
Sed <113764626+awkbashgrepsed@users.noreply.github.com>
2026-08-25 00:50:51 +0330

    Add player and vehicle god modes and remove old menu UI dependency

--- src/extras/modmenu.cpp
+++ src/extras/modmenu.cpp
@@ -9,6 +9,11 @@
 #include "Streaming.h"
 #include "Automobile.h"
 #include "ModelIndices.h"
+#include "Vehicle.h"
+
+#include <atomic>
+#include <chrono>
+#include <thread>
 
 namespace ReVCModMenu {
 
@@ -17,36 +22,28 @@ static float armour = 100.0f;
 static int32 weapon = WEAPONTYPE_COLT45;
 static int32 skin = 0;
 static int32 &money = CWorld::Players[0].m_nMoney;
+static std::atomic<bool> godMode{false};
+static std::atomic<bool> vehicleGodMode{false};
+static std::atomic<bool> running{true};
 
 static void ApplyHealth()
 {
-	CPlayerPed *ped = FindPlayerPed();
-	if (ped)
-		ped->m_fHealth = health;
+	CPlayerPed *ped = CWorld::Players[CWorld::PlayerInFocus].m_pPed;
+	if (ped) ped->m_fHealth = health;
 }
 
 static void ApplyArmour()
 {
-	CPlayerPed *ped = FindPlayerPed();
-	if (ped)
-		ped->m_fArmour = armour;
+	CPlayerPed *ped = CWorld::Players[CWorld::PlayerInFocus].m_pPed;
+	if (ped) ped->m_fArmour = armour;
 }
 
-static void FullHealth()
-{
-	health = 100.0f;
-	ApplyHealth();
-}
-
-static void FullArmour()
-{
-	armour = 100.0f;
-	ApplyArmour();
-}
+static void FullHealth() { health = 100.0f; ApplyHealth(); }
+static void FullArmour() { armour = 100.0f; ApplyArmour(); }
 
 static void GiveSelectedWeapon()
 {
-	CPlayerPed *ped = FindPlayerPed();
+	CPlayerPed *ped = CWorld::Players[CWorld::PlayerInFocus].m_pPed;
 	if (ped) {
 		ped->GiveWeapon((eWeaponType)weapon, 9999);
 		ped->SetCurrentWeapon((eWeaponType)weapon);
@@ -55,37 +52,27 @@ static void GiveSelectedWeapon()
 
 static void GiveAllWeapons()
 {
-	CPlayerPed *ped = FindPlayerPed();
-	if (ped) {
+	CPlayerPed *ped = CWorld::Players[CWorld::PlayerInFocus].m_pPed;
+	if (ped)
 		for (int32 i = WEAPONTYPE_BRASSKNUCKLE; i <= WEAPONTYPE_CAMERA; i++)
 			ped->GiveWeapon((eWeaponType)i, 9999);
-	}
 }
 
 static void ApplySkin()
 {
-	static const char *skinNames[] = {
-		"player", "cop", "swat", "fbi", "army", "medic", "fireman", "golf"
-	};
-
+	static const char *skinNames[] = { "player", "cop", "swat", "fbi", "army", "medic", "fireman", "golf" };
 	if (skin >= 0 && skin < (int32)(sizeof(skinNames) / sizeof(skinNames[0])))
-		CWorld::Players[0].SetPlayerSkin(skinNames[skin]);
+		CWorld::Players[CWorld::PlayerInFocus].SetPlayerSkin(skinNames[skin]);
 }
 
 static void SpawnVehicle(int32 modelId)
 {
 	CVector pos = FindPlayerCoors();
-
 	CStreaming::RequestModel(modelId, STREAMFLAGS_DONT_REMOVE);
 	CStreaming::LoadAllRequestedModels(true);
-
-	if (!CStreaming::HasModelLoaded(modelId))
-		return;
-
+	if (!CStreaming::HasModelLoaded(modelId)) return;
 	CAutomobile *vehicle = new CAutomobile(modelId, RANDOM_VEHICLE);
-	if (!vehicle)
-		return;
-
+	if (!vehicle) return;
 	pos.z += 0.5f;
 	vehicle->SetPosition(pos);
 	vehicle->SetHeading(FindPlayerHeading());
@@ -100,6 +87,24 @@ static void SpawnRhino() { SpawnVehicle(MI_RHINO); }
 static void SpawnPolice() { SpawnVehicle(MI_POLICE); }
 static void AddMoney() { money += 10000; }
 static void MaxMoney() { money = 99999999; }
+static void ToggleGodMode() { godMode = !godMode.load(); }
+static void ToggleVehicleGodMode() { vehicleGodMode = !vehicleGodMode.load(); }
+
+static void ProtectEntities()
+{
+	while (running.load(std::memory_order_relaxed)) {
+		CPlayerPed *ped = CWorld::Players[CWorld::PlayerInFocus].m_pPed;
+		if (ped) {
+			if (godMode.load()) {
+				ped->m_fHealth = 100.0f;
+				ped->m_fArmour = 100.0f;
+			}
+			if (vehicleGodMode.load() && ped->bInVehicle && ped->m_pMyVehicle && ped->m_pMyVehicle->pDriver == ped)
+				ped->m_pMyVehicle->m_fHealth = 1000.0f;
+		}
+		std::this_thread::sleep_for(std::chrono::milliseconds(20));
+	}
+}
 
 struct RegisterMenu {
 	RegisterMenu()
@@ -109,20 +114,21 @@ struct RegisterMenu {
 		DebugMenuAddCmd("Mod Menu|Player", "Max Money", MaxMoney);
 		DebugMenuAddCmd("Mod Menu|Player", "Full Health", FullHealth);
 		DebugMenuAddCmd("Mod Menu|Player", "Full Armour", FullArmour);
+		DebugMenuAddBool8("Mod Menu|Player", "God Mode", (int8*)&godMode, nil, nil);
 		DebugMenuAddFloat32("Mod Menu|Player", "Health Value", &health, nil, 10.0f, 0.0f, 100.0f);
 		DebugMenuAddCmd("Mod Menu|Player", "Apply Health", ApplyHealth);
 		DebugMenuAddFloat32("Mod Menu|Player", "Armour Value", &armour, nil, 10.0f, 0.0f, 100.0f);
 		DebugMenuAddCmd("Mod Menu|Player", "Apply Armour", ApplyArmour);
 		DebugMenuAddInt32("Mod Menu|Player", "Skin Index", &skin, nil, 1, 0, 7, nil);
 		DebugMenuAddCmd("Mod Menu|Player", "Apply Skin", ApplySkin);
-
 		DebugMenuAddInt32("Mod Menu|Weapons", "Weapon Index", &weapon, nil, 1, WEAPONTYPE_UNARMED, WEAPONTYPE_CAMERA, nil);
 		DebugMenuAddCmd("Mod Menu|Weapons", "Give Selected Weapon", GiveSelectedWeapon);
 		DebugMenuAddCmd("Mod Menu|Weapons", "Give All Weapons", GiveAllWeapons);
-
+		DebugMenuAddBool8("Mod Menu|Vehicles", "God Mode", (int8*)&vehicleGodMode, nil, nil);
 		DebugMenuAddCmd("Mod Menu|Spawn", "Spawn Infernus", SpawnInfernus);
 		DebugMenuAddCmd("Mod Menu|Spawn", "Spawn Rhino", SpawnRhino);
 		DebugMenuAddCmd("Mod Menu|Spawn", "Spawn Police", SpawnPolice);
+		std::thread(ProtectEntities).detach();
 	}
 };
 
